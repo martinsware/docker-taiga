42453dca7fd01835a275cacad0de5e6f825b7476
Ben Yanke <byanke@pcnametag.com>
2018-06-25 15:42:58 -0500

    Adding db wait loop to speed up provisioning - now only waits as long as needed instead of a long set wait

--- checkdb.py
+++ checkdb.py
@@ -1,4 +1,4 @@
-import os, sys, psycopg2
+import os, sys, psycopg2, time
 
 DB_NAME = os.getenv('TAIGA_DB_NAME')
 DB_HOST = os.getenv('TAIGA_DB_HOST')
@@ -14,9 +14,25 @@ print("Connecting to database:\n" + conn_string)
 conn = psycopg2.connect(conn_string)
 cur = conn.cursor()
 
-cur.execute("select * from information_schema.tables where table_name=%s", ('django_migrations',))
-exists = bool(cur.rowcount)
+tryLimit = 60
+tryCount = 0
+sleepSeconds = 1
 
-if exists is False:
-    print("Database does not appear to be setup.")
-    sys.exit(2)
+# Keep trying to connect until the retry limit is reached or connection success
+while True:
+    cur.execute("select * from information_schema.tables where table_name=%s", ('django_migrations',))
+    exists = bool(cur.rowcount)
+
+    if exists is False:
+
+        if tryCount++ < tryLimit:
+            print("Database is not yet ready. Connection attempt " + tryCount + " of " tryLimit + ". Sleeping for " + sleepSeconds + " seconds and trying again.\n")
+            time.sleep(sleepSeconds)
+        else:
+            print("\n\nDatabase wait timeout reached after " + tryLimit + " attempts. Exiting.")
+            sys.exit(2)
+
+    else:
+        print("Database is ready. Continuing with setup.\n\n")
+        sys.exit(0)
+
